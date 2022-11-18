21e1b85984a7b737d83357e7072e40b196264b12
pedrinho123 <pedrosilvafarias@gmail.com>
2022-11-17 22:06:16 -0300

    dqhudhiahuidhisad

--- sniff.py
+++ sniff.py
@@ -3,6 +3,16 @@
 from scapy.all import *
 
 def example(pkt):
-        print('teste')
+        #get the source and destination IP addresses
+        src = pkt[IP].src
+        dst = pkt[IP].dst
+        if(dst == "10.2.2.1"):
+                print("Packet from source: " + src + " to destination: " + dst) 
+                pkt.show()
+                return pkt
+        elif(dst == "10.1.1.1"):
+                print("Packet from source: " + src + " to destination: " + dst)
+                pkt.show()
+                return None
 
 sniff(iface=['r-eth1','r-eth2'], prn=example)
