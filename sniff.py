6047a39f9761541b040a4dbe92d5e30864de643d
pedrinho123 <pedrosilvafarias@gmail.com>
2022-11-17 22:10:12 -0300

    1231

--- sniff.py
+++ sniff.py
@@ -6,13 +6,13 @@ def example(pkt):
         #get the source and destination IP addresses
         src = pkt[IP].src
         dst = pkt[IP].dst
-        if(dst == "10.2.2.1"):
+        """if(dst == "10.2.2.1"):
                 print("Packet from source: " + src + " to destination: " + dst) 
                 pkt.show()
                 return pkt
         elif(dst == "10.1.1.1"):
                 print("Packet from source: " + src + " to destination: " + dst)
                 pkt.show()
-                return None
+                return None"""
 
 sniff(iface=['r-eth1','r-eth2'], prn=example)
