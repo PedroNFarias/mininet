dbc900bb29deadaaa2fd6980d651a43a8e149a0f
pedrinho123 <pedrosilvafarias@gmail.com>
2022-11-19 17:26:18 -0300

    123

--- sniff.py
+++ sniff.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 
 from scapy.all import *
+from scapy.layers.inet import IP, TCP
+from scapy.layers.l2 import Ether
+from scapy.layers import all
+from scapy import *
 
 def forwarder(pkt):
-        print(pkt)
+        """source_mac_address = pkt[Ether].src
+        destination_mac_address = pkt[Ether].dst
+
+        destination_address = pkt[IP].dst
+        source_address = pkt[IP].src"""
+        mac = getmacbyip(pkt[IP].dst)
+        #send the packet to the destination by spoofing the source mac address
+        sendp(Ether(src=mac)/IP(dst=pkt[IP].dst)/TCP(dport=pkt[TCP].dport, sport=pkt[TCP].sport, flags=pkt[TCP].flags, seq=pkt[TCP].seq, ack=pkt[TCP].ack)/pkt[TCP].payload, iface="eth0")
+
+        
 
 
 sniff(iface=['r-eth1','r-eth2'], prn=forwarder)
