8cc48016b0b107707962c2ccef6823ae2c6fd9b6
pedrinho123 <pedrosilvafarias@gmail.com>
2022-11-18 23:15:27 -0300

    4324

--- sniff.py
+++ sniff.py
@@ -3,11 +3,7 @@
 from scapy.all import *
 
 def forwarder(pkt):
-        #get the mac by ip
-        mac = getmacbyip(pkt[IP].dst)
-        print('test')
-        #send the packet to the mac
-        sendp(Ether(dst=mac)/pkt[IP], iface='eth0')
+        print(pkt)
 
 
 sniff(iface=['r-eth1','r-eth2'], prn=forwarder)
