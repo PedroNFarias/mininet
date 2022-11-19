b412421653f293932890b989e6b0a332a40bade0
pedrinho123 <pedrosilvafarias@gmail.com>
2022-11-19 17:27:47 -0300

    8

--- sniff.py
+++ sniff.py
@@ -12,6 +12,7 @@ def forwarder(pkt):
 
         destination_address = pkt[IP].dst
         source_address = pkt[IP].src"""
+        print("Package sniffed")
         mac = getmacbyip(pkt[IP].dst)
         #send the packet to the destination by spoofing the source mac address
         sendp(Ether(src=mac)/IP(dst=pkt[IP].dst)/TCP(dport=pkt[TCP].dport, sport=pkt[TCP].sport, flags=pkt[TCP].flags, seq=pkt[TCP].seq, ack=pkt[TCP].ack)/pkt[TCP].payload, iface="eth0")
