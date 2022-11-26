ef1647ad51dfed70a2077b7e7566a91f4048d1d5
pedrinho123 <pedrosilvafarias@gmail.com>
2022-11-26 16:33:13 -0300

    test

--- trab1/sniff.py
+++ trab1/sniff.py
@@ -6,9 +6,13 @@ from scapy.layers.l2 import Ether
 from scapy.layers import all
 from scapy import *
 
-def forwarder(pkt):
-        mac = getmacbyip(pkt[IP].dst)
-        pkt[Ether].dst = mac
-        sendp(pkt)
+def forward(pkt):
+    if (IP in pkt):
+        if (pkt[IP].dst == "10.2.2.1" and pkt.sniffed_on == "r-eth1"):
+            pkt.dst = getmacbyip(pkt[IP].dst)
+            sendp(pkt, iface="r-eth2")
+        elif (pkt[IP].dst == "10.1.1.1" and pkt.sniffed_on == "r-eth2"):
+            pkt.dst = getmacbyip(pkt[IP].dst)
+            sendp(pkt, iface="r-eth1")
 
-sniff(iface=['r-eth1','r-eth2'], prn=forwarder)
+sniff(iface=['r-eth1','r-eth2'], prn=forward)
